bdaeb81dff859e51c4442b3595e572ecc90d9849
thorDemo <40915413+thorDemo@users.noreply.github.com>
2020-01-08 11:03:21 +0800

    抽奖页面更新

---
+++ src/components/page/chouJiang.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <span>抽奖测试页面</span>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+<style>
+
+</style>
