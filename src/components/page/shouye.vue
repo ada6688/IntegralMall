b7a7a50fdade3cb46cc3353002fb25f4a3ac5dfe
Ada-Su <42989161+Ada-Su@users.noreply.github.com>
2020-01-09 17:35:06 +0800

    首页

--- src/components/page/shouye.vue
+++ src/components/page/shouye.vue
@@ -31,34 +31,26 @@
         <el-row>
           <el-tabs type="border-card">
             <el-tab-pane label="推荐">
-              <vuescroll>
-                <div v-for="goods in recommend" v-bind:key="goods.item" class="products-item">
-                  <div class="product-item-text">
-                    <p>{{goods.title}}</p>
-                    <span>{{goods.points}}积分</span>
-                  </div>
-                  <div class="product-item-img">
-                    <img :src="'http://45.64.53.115:8000'+goods.img_url.url" alt="">
-                  </div>
+              <div v-for="goods in recommend" v-bind:key="goods.item" class="products-item">
+                <div class="product-item-text">
+                  <p>{{goods.title}}</p>
+                  <span>{{goods.points}}积分</span>
                 </div>
-              </vuescroll>
+                <div class="product-item-img">
+                  <img :src="'http://45.64.53.115:8000'+goods.img_url.url" alt="">
+                </div>
+              </div>
             </el-tab-pane>
             <el-tab-pane label="数码">
-              <vuescroll
-              :refreshStart='refreshStart'
-              :loadStart='loadStart'
-              :noData='noData'
-              >
-                <div v-for="goods in digital" v-bind:key="goods.item" class="products-item">
-                  <div class="product-item-text">
-                    <p>{{goods.title}}</p>
-                    <span>{{goods.points}}积分</span>
-                  </div>
-                  <div class="product-item-img">
-                    <img :src="'http://45.64.53.115:8000'+goods.img_url.url" alt="">
-                  </div>
+              <div v-for="goods in digital" v-bind:key="goods.item" class="products-item">
+                <div class="product-item-text">
+                  <p>{{goods.title}}</p>
+                  <span>{{goods.points}}积分</span>
                 </div>
-              </vuescroll>
+                <div class="product-item-img">
+                  <img :src="'http://45.64.53.115:8000'+goods.img_url.url" alt="">
+                </div>
+              </div>
             </el-tab-pane>
             <el-tab-pane label="奖金">
               <div v-for="goods in bonus" v-bind:key="goods.item" class="products-item">
