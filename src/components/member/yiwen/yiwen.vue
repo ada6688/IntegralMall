03508a1876b109f2762d72b5380493669a106a39
summer-rue <59637117+summer-rue@users.noreply.github.com>
2020-01-11 16:22:13 +0800

    1-11 yiwen.vue
    常见疑问修改

--- src/components/member/yiwen/yiwen.vue
+++ src/components/member/yiwen/yiwen.vue
@@ -1,21 +1,157 @@
 <template>
   <div>
     <TopNavC></TopNavC>
-    <p>常见疑问</p>
-    <p>常见疑问</p>
-    <p>常见疑问</p>
-    <p>常见疑问</p>
-    <p>常见疑问</p>
-    <p>常见疑问</p>
-    <p style="color:#ffffff" @click="goBack()">返回</p>
-  </div>
+        <!-- rule开始 -->
+        <div id='rule'>
+            <!-- 页面上方图片 -->
+            <img src="" alt="" id="rulepic">
+
+            <!-- 中间规则内容部分开始 -->
+            <div id='ruleBox'>
+                <el-tabs type="border-card" >
+                    <!--正文！ 商城积分问题 -->
+                    <el-tab-pane label="商城积分问题">
+                        <el-collapse accordion>
+                            <el-collapse-item>
+                              <template slot="title">
+                                                                        <!-- 商城积分问题1 -->
+                                <i class="header-icon el-icon-collection-tag"></i>我是否需要注册参与宝马会的奖励积分计划？
+                              </template>
+                                                    <!-- 内容 -->
+                              <p>宝马会的积分奖励无需特别申请，成为宝马会的会员后，将自动成为宝马会商城会员，所有有效投注都将按照400:1的比例计算成积分，并派发到您的积分账户。</p>
+
+                            </el-collapse-item>
+                            <!-- 一条规则结束！！ -->
+
+                            <el-collapse-item>
+                                <template slot="title">
+                                                                          <!-- 商城积分问题2 -->
+                                  <i class="header-icon el-icon-collection-tag"></i>我的奖励积分是否会过期？
+                                </template>
+                                                      <!-- 内容 -->
+                                <p>是的。宝马会的积分的有效期如下所示：   普通会员积分有效期为 6 个月，VIP会员的积分有效期为 12个月。任何未使用的过期积分将会被清除。因此，请在过期之前及时兑换您的积分，您可以点击"积分过期报表"   查询即将过期的积分。</p>
+  
+                              </el-collapse-item>
+                              <!-- 一条规则结束！！ -->
+
+                              <el-collapse-item>
+                                <template slot="title">
+                                                                          <!-- 商城积分问题3 -->
+                                  <i class="header-icon el-icon-collection-tag"></i>如果商城没有我喜欢的礼物，怎么办？
+                                </template>
+                                                      <!-- 内容 -->
+                                <p>宝马会商城将会时常更新可兑换的物品，若会员在商城的物品列表中没有发现喜欢的物品，可以给我们留言，我们将充分参考会员的意见，及时更新受会员喜爱的物品。</p>
+  
+                              </el-collapse-item>
+                              <!-- 一条规则结束！！ -->
+
+                              <el-collapse-item>
+                                <template slot="title">
+                                                                          <!-- 商城积分问题4 -->
+                                  <i class="header-icon el-icon-collection-tag"></i>积分兑换礼品后是否可以取消兑换？
+                                </template>
+                                                      <!-- 内容 -->
+                                <p>可以。会员申请兑换礼品后，若订单尚未处理，可立即联系客服申请取消，相应积分将会返还至会员的积分账户。但如果订单已经处理，相应礼品已在派送中，则无法取消。</p>
+  
+                              </el-collapse-item>
+                              <!-- 一条规则结束！！ -->
+
+                              <el-collapse-item>
+                                <template slot="title">
+                                                                          <!-- 商城积分问题5 -->
+                                  <i class="header-icon el-icon-collection-tag"></i>我是否需要注册参与宝马会的奖励积分计划？
+                                </template>
+                                                      <!-- 内容 -->
+                                <p>会员申请兑换礼品后，宝马会将在7个工作日之内对会员的兑换情况进行审核，审核无误后将会为会员派发礼品。</p>
+  
+                              </el-collapse-item>
+                              <!-- 一条规则结束！！ -->
+
+                          </el-collapse>
+                          
+                    </el-tab-pane>
+
+                    <!--正文！ 礼品快递问题 -->
+                    <el-tab-pane label="礼品快递问题">
+                        <el-collapse accordion>
+                            <el-collapse-item>
+                                <template slot="title">
+                                                                          <!-- 礼品快递问题1 -->
+                                  <i class="header-icon el-icon-collection-tag"></i>我的快递为什么一直不到？
+                                </template>
+                                                      <!-- 内容 -->
+                                <p>会员可根据自己订单的快递单号在对应的快递网站内查询派送情况，若查询无效或始终无法收到，可联系宝马会商城客服，我们将协助会员与快递方沟通。</p>
+  
+                              </el-collapse-item>
+                              <!-- 一条规则结束！！ -->
+  
+                              <el-collapse-item>
+                                  <template slot="title">
+                                                                            <!-- 礼品快递问题2 -->
+                                    <i class="header-icon el-icon-collection-tag"></i>我的快递单号在哪看？
+                                  </template>
+                                                        <!-- 内容 -->
+                                  <p>您可在【账户中心】内看到自己订单状态，但是无法查看快递单号</p>
+    
+                                </el-collapse-item>
+                                <!-- 一条规则结束！！ -->
+  
+                                <el-collapse-item>
+                                  <template slot="title">
+                                                                            <!-- 礼品快递问题3 -->
+                                    <i class="header-icon el-icon-collection-tag"></i>我可否选择自己喜欢的快递方式？
+                                  </template>
+                                                        <!-- 内容 -->
+                                  <p>一般情况下快递都由默认的快递方式送达，贵重物品均会采用顺丰快递发送，新疆、甘肃、西藏、内蒙地区多采用EMS快递发送，若会员有特殊的快递要求可提前声明，但相应的运费差价需由会员自行负责。</p>
+    
+                                </el-collapse-item>
+                                <!-- 一条规则结束！！ -->
+  
+                                <el-collapse-item>
+                                  <template slot="title">
+                                                                            <!-- 礼品快递问题4 -->
+                                    <i class="header-icon el-icon-collection-tag"></i>必须填写真实的收货信息吗？
+                                  </template>
+                                                        <!-- 内容 -->
+                                  <p>是的。会员在宝马会商城兑换的奖品，一定要填写真实的收货信息，若因收货信息错误而导致的快递失败、退回、商品超过保质期（保修期）等情况，由会员自行承担相应责任。</p>
+    
+                                </el-collapse-item>
+                                <!-- 一条规则结束！！ -->
+  
+                                <el-collapse-item>
+                                  <template slot="title">
+                                                                            <!-- 礼品快递问题5 -->
+                                    <i class="header-icon el-icon-collection-tag"></i>兑换的礼品什么时候寄出？
+                                  </template>
+                                                        <!-- 内容 -->
+                                  <p><p class="dispp" style="display: block;">礼品寄出的时间以会员的下单时间为准 —— 自订单提交起，1-7个工作日内处理订单；节假日期间可能会根据快递部门的营业时间做出调整，原则上以发货时间为准。</p></p>
+    
+                                </el-collapse-item>    
+                        </el-collapse>      
+                    </el-tab-pane>
+                  </el-tabs>
+            </div>
+             
+            <!-- 页面底部 -->
+            <!-- <div id="ruleBottomLine">
+                <hr>
+                <p>还有任何疑问？宝马会欢迎您随时联系 <a href="">在线客服</a></p>
+            </div> -->
+        </div>
+        <!-- rule结束 -->
+    </div>
 </template>
 <script>
-import TopNavC from '@/components/common/TopNavC'
-
+  import TopNavC from '@/components/common/TopnavC'
 export default {
+  
   components: {
     TopNavC
+  },
+  data () {
+    return {
+      activeNames: ['1']
+    }
   },
   mounted: function () {
     this.change()
@@ -37,5 +173,43 @@ export default {
 
 </script>
 <style>
+  #rule {
+      margin-top: 3rem;
+      height: 650px;
+  }
+  #ruleBottomLine{
+      bottom: 0;
+      text-align: center;
+  
+  }
+  #ruleBox{
+      text-align: justify;
+  }
 
+  #ruleBox i{
+      margin: 0 1rem;
+  }
+  #rule .el-icon-arrow-right{
+      margin-top: -4px;
+  }
+  #rule .el-collapse-item__header{
+      background-color: #151515;
+      border-bottom: 2px solid #2b2a2f;
+      color: #fefeff;
+  }
+  #rule .el-collapse-item__content{
+      background-color:#1b1b1b ;
+      color: #fefeff;
+  }
+  #rule .el-tabs--border-card>.el-tabs__content{
+      background-color:#1b1b1b ;
+  }
+  #rule .el-collapse{
+      background-color: #1b1b1b;
+      border-top:1px solid #151515
+  }
+
+  #ruleBox p{
+      padding: 1rem;
+  }
 </style>
