4355c9ea09e64dcc5211d827f5ac8e521c07b7cb
Ada-Su <42989161+Ada-Su@users.noreply.github.com>
2020-01-14 16:53:13 +0800

    Update order.vue

--- src/components/page/order.vue
+++ src/components/page/order.vue
@@ -2,12 +2,54 @@
   <div>
     <TopNavC></TopNavC>
     <div class="main-second-wrap">
-      <div class="order-product-detail">
-        <img src="../../assets/images/products/bsbb.png" alt="">
+      <div class="order-product-wrap">
+        <div class="order-img">
+          <img src="../../assets/images/products/bsbb.png" alt="">
+        </div>
+        <div class="order-text">
+          <p>您要兑换的商品是</p>
+          <span class="order-pro-detail">Apple iPhone 11 Pro Max (A2220) 256G B 联通电信4G手机 双卡双待！</span>
+          <div class="order-pro-cost">
+            <p>
+              <span>原积分</span>
+              <span>4000</span>
+            </p>
+            <p>
+              <span>折扣价</span>
+              <span>3680</span>
+            </p>
+          </div>
+        <div class="order-pro-detail-button"><p>查看商品详情</p></div>
+        </div>
       </div>
       <div class="order-form">
-        <p>您要兑换的商品是</p>
-        <p></p>
+        <form @submit="submit()">
+          <div>
+            <span>*</span><span class="order-name-T">姓名</span><span>:</span>
+            <input type="text" placeholder="请输入您的真实姓名">
+          </div>
+          <div>
+            <span>*</span><span class="order-name-T">用户名</span><span>:</span>
+            <input type="text" placeholder="请输入您的用户名">
+          </div>
+          <div>
+            <span>*</span><span class="order-name-T">邮箱</span><span>:</span>
+            <input type="text" placeholder="请输入您的邮箱">
+          </div>
+          <div>
+            <span>*</span><span class="order-name-T">电话</span><span>:</span>
+            <input type="text" placeholder="请输入您的电话">
+          </div>
+          <div>
+            <span>*</span><span class="order-name-T">微信号</span><span>:</span>
+            <input type="text" placeholder="请输入您的微信号">
+          </div>
+          <div>
+            <span>*</span><span class="order-name-T">邮寄地址</span><span>:</span>
+            <input type="text" placeholder="虚拟商品无须填写地址">
+          </div>
+          <button>提交</button>
+        </form>
       </div>
     </div>
   </div>
