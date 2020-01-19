69cf2b47ca7825199fdc65fd371da39272b454b1
perry007 <42998546+perry007@users.noreply.github.com>
2020-01-19 11:53:59 +0800

    搜索页面更新
    搜索页面更新

---
+++ src/components/page/searchData.vue
@@ -0,0 +1,145 @@
+<template>
+  <div>
+    <TopNavC></TopNavC>
+    <div class="main-second-wrap">
+        <div class="search-main">
+        <input type="text" class="search-box" v-model="inWord" placeholder="请输入商品名称">
+        <button class="search-btn" @click="SearchAgain">搜索</button>
+        </div>
+        <div class="recommend-wrap1">
+        <div>
+          <div class="items-list" v-if="isGet=='no'">            
+            <div class="items-pro" v-for="goods in SearchData" v-bind:key="goods.pk" @click="jumpOrder(goods.pk)">
+                <div class="item-img-wrap"  v-if="goods.app_img">
+                    <img :src="'http://45.64.53.115:8000' + goods.app_img.url" alt="">
+                </div>
+                <div class="item-img-wrap"  v-else>
+                    <img :src="'http://45.64.53.115:8000' + goods.pc_img.url" alt="">
+                </div>
+                <div class="item-pro-text">
+                  <b>{{ goods.title }}</b>
+                  <p>{{ goods.description }}</p>
+                  <span>{{ goods.points }}积分</span>
+                </div>
+            </div>            
+          </div>
+          <div v-else-if="isGet=='no'">
+              <p style="color:#FFFFFF">关于&nbsp;<span style="color:red">"{{ searchWord }}"</span>&nbsp;的搜索结果不存在</p>              
+          </div>
+          <div v-else>              
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- <Result></Result> -->    
+  </div>
+</template>
+<script>
+import TopNavC from '@/components/common/TopNavC'
+import axios from 'axios'
+import vuescroll from 'vuescroll'
+// import Result from '@/components/page/result'
+export default {
+  components: {
+    TopNavC,      
+    vuescroll    
+  },  
+  mounted: function () {
+    this.change()
+  },
+  data() {
+      return {
+        searchWord: this.$route.params.word,
+        SearchData: [],  
+        inWord: '',
+        notice: '',
+        isGet: ''
+      }
+  },  
+  methods: {
+    goBack () {
+      if (window.history.length <= 1) {
+        this.$router.push({path: '/'})
+        return false
+      } else {
+        this.$router.go(-1)
+      }
+    },
+    change () {
+      document.getElementById('top-logo-change').innerHTML = '<p>搜索结果</P>'
+    },
+    refreshStart (done) {
+      setTimeout(() => {
+        done()
+      }, 1500)
+    },    
+    // 加载开始
+    loadStart (done) {
+      setTimeout(() => {
+        done()
+      }, 1500)
+    },        
+    SearchAgain () {        
+        axios.get('http://45.64.53.115:8000/api/mulu/?format=json&search=' + this.inWord)
+      .then(Response => {         
+        this.SearchData = Response.data.results;
+        window.search = this.inWord
+        this.isGet = this.SearchData.length == 0 ? 'yes' : 'no'      
+      })
+      .catch(error => {                  
+        console.log(error)         
+        // alert('商品加载错误，请联系在线客服！')
+      })
+     },
+     jumpOrder (id) {
+      window.orderid = id
+      this.$router.push('/order')
+      }     
+    },
+created () {     
+    axios.get('http://45.64.53.115:8000/api/mulu/?format=json&search=' + window.search)
+      .then(Response => {                   
+        this.SearchData = Response.data.results;
+        this.isGet = this.SearchData.length == 0 ? 'yes' : 'no'
+      })
+      .catch(error => {           
+        console.log(error)            
+        // alert('商品加载错误，请联系在线客服！')
+      }) 
+  },  
+}
+</script>
+<style scoped>
+.search-main{
+    margin:16.67pt 17pt 0 16.67pt;
+    height: 36.67px;
+}
+.search-box{
+    background: #2A2C35;
+    color: #ffffff;
+    position: absolute;    
+    left: 16.67px;
+    width: 70%;
+    height: 36.67px;
+    border: none;        
+    padding-left: 10px;
+}
+input:focus{
+    outline: none;  
+}
+.search-btn{
+    font-size: 15.33px;
+    width: 15%;
+    height: 36.67px;
+    margin-left: 16.67px;
+    position: absolute;
+    right: 17px;
+    text-align: center;
+    background: #B58024;   
+    color: #FFFFFF; 
+    border: none;
+    border-radius: 0%;
+    font-family: "PingFangSC";    
+}
+
+</style>
